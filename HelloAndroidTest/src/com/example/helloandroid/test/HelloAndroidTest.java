305922aa22b47016462fd8032569f8b831c8ef7e
Ohminami.masato <dainan@ntts-sv.co.jp>
2013-04-22 17:54:13 +0900

    jenkins確認用
    Helloworldテストプログラム

---
+++ HelloAndroidTest/src/com/example/helloandroid/test/HelloAndroidTest.java
@@ -0,0 +1,29 @@
+package com.example.helloandroid.test;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+import com.example.helloandroid.HelloAndroid;
+
+public class HelloAndroidTest extends
+		ActivityInstrumentationTestCase2<HelloAndroid> {
+		private HelloAndroid mActivity;
+		private TextView mView;
+		private String resourceString;
+
+
+	public HelloAndroidTest(){
+		super("com.example.helloandroid",HelloAndroid.class);
+	}
+	 @Override
+	protected void setUp()  throws  Exception{
+		super.setUp();
+		mActivity=this.getActivity();
+		mView=(TextView) mActivity.findViewById(com.example.helloandroid.R.id.textview);
+		resourceString =mActivity.getString(com.example.helloandroid.R.string.hello_world);
+	}
+	public void testPreconditions(){
+		assertNotNull(mView);
+	}
+	public void testText(){
+		assertEquals(resourceString,(String)mView.getText());
+	}
+}
